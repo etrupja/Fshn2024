cb4abdd996f7c45ced5fd47952faa4635f14c1a5
“Ervis <ervistrupja@outlook.com>
2024-04-23 10:43:14 +0200

    added api

---
+++ Student.API/Controllers/StudentsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Student.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+
+        [HttpGet("All")]
+        public IActionResult GetStudents()
+        {
+            return Ok();
+        }
+
+        [HttpGet("GetById/{id}")]
+        public IActionResult GetStudentById(int id)
+        {
+            return Ok($"Student with id = {id}");
+        }
+
+
+        [HttpPost]
+        public IActionResult AddNewStudent([FromBody]object payload)
+        {
+            return Ok("Student added");
+        }
+
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateStudent([FromBody] object payload, int id)
+        {
+            return Ok("Student added");
+        }
+
+
+        [HttpDelete("DeleteById/{id}")]
+        public IActionResult DeleteStudent(int id)
+        {
+            return Ok();
+        }
+
+    }
+}
